b166c2a63611891677cff8b28c0b6caafd81bce2
alexandre-ia <alexandrehenriquepereira310@gmail.com>
2026-01-24 12:33:40 -0300

    feat: Logica de cadastro de item

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,13 +1,37 @@
 package com.example.cardapio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/food")
 public class FoodController {
 
+    @Autowired
+    private FoodRepository foodRepository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodDate = new Food(data);
+        foodRepository.save(foodDate);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public void getAllFood() {}
+    public List<FoodResponseDTO> getAllFood() {
+        List<FoodResponseDTO> foodList = foodRepository.findAll()
+                .stream()
+                .map(FoodResponseDTO::new)
+                .toList();
+        return foodList;
+    }
 }
